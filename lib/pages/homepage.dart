7248f61de7a6d805b6b1c3709e26e5d8f17e007e
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2023-02-07 10:04:55 +0200

    Added functionality to the navigation bar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/material.dart';
 
-class homePage extends StatelessWidget {
+class homePage extends StatefulWidget {
   const homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  final List<Widget> _screens = [
+    const Center(
+      child: Text('Ako Home'),
+    ),
+    const Center(
+      child: Text('Ako Events'),
+    ),
+    const Center(
+      child: Text('Ako Add'),
+    ),
+    const Center(
+      child: Text('Ako Community'),
+    ),
+    const Center(
+      child: Text('Ako Profile'),
+    ),
+  ];
+  int _currentIndex = 0;
+
+  void _currentIndexSelector(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Ako App'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.wallet), label: 'Events'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Community'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.person_2_rounded), label: 'Profile'),
-      ]),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.white,
+          backgroundColor: Colors.pink,
+          elevation: 0,
+          unselectedItemColor: Colors.white,
+          currentIndex: _currentIndex,
+          onTap: _currentIndexSelector,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.wallet), label: 'Events'),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), label: 'Community'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_rounded), label: 'Profile'),
+          ]),
     );
   }
 }
