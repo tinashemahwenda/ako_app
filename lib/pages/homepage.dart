641dc3e4c10e83ed7fd621d7924a38e6c4eb3a64
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2023-02-07 09:43:25 +0200

    Created a bottom navigation bar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,10 +5,18 @@ class homePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text('Ako App'),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.wallet), label: 'Events'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Community'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person_2_rounded), label: 'Profile'),
+      ]),
     );
   }
 }
